ce2f2a4d7a0311d5bb0e90f2562040e23737d5cd
qyinm <qusseun@gmail.com>
2024-11-11 08:37:32 +0900

    feat: 유저 의사 묻는 입력 구조 유효 패턴 상수 작성

--- src/main/java/store/constants/view/InputValidatePatternConstants.java
+++ src/main/java/store/constants/view/InputValidatePatternConstants.java
@@ -3,7 +3,8 @@ package store.constants.view;
 import java.util.regex.Pattern;
 
 public enum InputValidatePatternConstants {
-    VALID_INPUT_ITEM_PATTERN("^\\[[ㄱ-ㅎ가-힣a-zA-Z]+\\-([1-9][0-9]*)\\](,\\[[ㄱ-ㅎ가-힣a-zA-Z]+\\-([1-9][0-9]*)\\])*");
+    VALID_INPUT_ITEM_PATTERN("^\\[[ㄱ-ㅎ가-힣a-zA-Z]+\\-([1-9][0-9]*)\\](,\\[[ㄱ-ㅎ가-힣a-zA-Z]+\\-([1-9][0-9]*)\\])*"),
+    VALID_INPUT_ASK_ANSWER("[Y|N]");
 
     private final Pattern validatePattern;
 
