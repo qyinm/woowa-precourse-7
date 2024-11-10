e4fcaea059f89a7939c157465734a11b3e3efffa
qyinm <qusseun@gmail.com>
2024-11-11 08:38:19 +0900

    refactor(OutputViewConstants): 필드명 오타 수정

--- src/main/java/store/constants/view/OutputViewConstants.java
+++ src/main/java/store/constants/view/OutputViewConstants.java
@@ -6,17 +6,18 @@ public enum OutputViewConstants {
             현재 보유하고 있는 상품입니다.
             """),
     USER_ITEM_INPUT_PREFIX("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
+    ASK_GET_MORE_BONUS_PRODUCTS("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
     ASK_FULL_PRICE_PURCHASE_PREFIX("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
     ASK_GET_MEMBERSHIP_DISCOUNT_PREFIX("멤버십 할인을 받으시겠습니까? (Y/N)"),
     ASK_MORE_SHOPPING_PREFIX("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
 
-    private final String ouputViewPrefix;
+    private final String outputViewPrefix;
 
-    OutputViewConstants(String ouputViewPrefix) {
-        this.ouputViewPrefix = ouputViewPrefix;
+    OutputViewConstants(String outputViewPrefix) {
+        this.outputViewPrefix = outputViewPrefix;
     }
 
-    public String getOuputViewPrefix() {
-        return ouputViewPrefix;
+    public String getOutputViewPrefix() {
+        return outputViewPrefix;
     }
 }
