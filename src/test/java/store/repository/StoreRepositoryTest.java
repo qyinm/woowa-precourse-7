8fa0739c3109d9a77c24e91784b8de6c56bffedd
qyinm <qusseun@gmail.com>
2024-11-10 17:23:18 +0900

    test: 일반, 프로모션 상품 재고 수량 업데이트 기능 테스트 작성

--- src/test/java/store/repository/StoreRepositoryTest.java
+++ src/test/java/store/repository/StoreRepositoryTest.java
@@ -40,10 +40,42 @@ public class StoreRepositoryTest {
         storeRepository = new StoreRepository(Set.of(product));
 
         // When
-        Optional<Product> generalProduct = storeRepository.findPromotionProductByName(product.getName());
+        Optional<Product> promotionProduct = storeRepository.findPromotionProductByName(product.getName());
 
         // Then
-        assertThat(generalProduct).isPresent();
-        assertThat(generalProduct.get().getPrice()).isEqualTo(product.getPrice());
+        assertThat(promotionProduct).isPresent();
+        assertThat(promotionProduct.get().getPrice()).isEqualTo(product.getPrice());
+    }
+
+    @Test
+    @DisplayName("일반 상품 수량 변경 테스트")
+    void 일반_상품_수량_변경_테스트() {
+        // Given
+        Product product = new Product("땅콩", BigDecimal.valueOf(1000), BigDecimal.valueOf(100), null);
+        storeRepository = new StoreRepository(Set.of(product));
+
+        // When
+        Product generalProduct = storeRepository.updateGeneralProduct(product.getName(), BigDecimal.valueOf(90));
+
+        // Then
+        assertThat(generalProduct.getPrice()).isEqualTo(product.getPrice());
+        assertThat(generalProduct.getQuantity()).isEqualTo(BigDecimal.valueOf(90));
+    }
+
+    @Test
+    @DisplayName("프로모션 상품 수량 변경 테스트")
+    void 프로모션_상품_수량_변경_테스트() {
+        // Given
+        Promotion promotion = new Promotion("1+2", BigDecimal.valueOf(1), BigDecimal.valueOf(2), LocalDate.MIN,
+                LocalDate.MAX);
+        Product product = new Product("땅콩", BigDecimal.valueOf(1000), BigDecimal.valueOf(100), promotion);
+        storeRepository = new StoreRepository(Set.of(product));
+
+        // When
+        Product promotionProduct = storeRepository.updatePromotionProduct(product.getName(), BigDecimal.valueOf(90));
+
+        // Then
+        assertThat(promotionProduct.getPrice()).isEqualTo(product.getPrice());
+        assertThat(promotionProduct.getQuantity()).isEqualTo(BigDecimal.valueOf(90));
     }
 }
