53d4d1e3216807baab775a32646995b1601a22f9
qyinm <qusseun@gmail.com>
2024-11-11 08:37:01 +0900

    refactor(ItemInputDto): price 변수명 quantity 로 용도에 맞는 변수명으로 변경

--- src/main/java/store/dtos/ItemInputDto.java
+++ src/main/java/store/dtos/ItemInputDto.java
@@ -2,5 +2,5 @@ package store.dtos;
 
 import java.math.BigDecimal;
 
-public record ItemInputDto(String itemName, BigDecimal price) {
+public record ItemInputDto(String itemName, BigDecimal quantity) {
 }
