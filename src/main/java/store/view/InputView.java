badbb51f37b7bcc10945db73cd4446624432ec74
qyinm <qusseun@gmail.com>
2024-11-11 19:59:55 +0900

    fix(InputView): 유저 멤버십 할인 여부 입력 시 나오는 출력문 추가 작성

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -62,6 +62,8 @@ public class InputView {
     }
 
     public static boolean askGetMembershipDiscount() {
+        OutputView.printAskGetMembershipDiscountPrefix();
+
         return askYesOrNo();
     }
 
