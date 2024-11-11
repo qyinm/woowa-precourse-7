3918100ab448f4f7efb7875421ee3c98dc328281
qyinm <qusseun@gmail.com>
2024-11-11 20:11:22 +0900

    refactor(OutputView): 영수증 출력 메서드 분리

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,6 +1,11 @@
 package store.view;
 
-import static store.constants.view.OutputViewConstants.*;
+import static store.constants.view.OutputViewConstants.ASK_FULL_PRICE_PURCHASE_PREFIX;
+import static store.constants.view.OutputViewConstants.ASK_GET_MEMBERSHIP_DISCOUNT_PREFIX;
+import static store.constants.view.OutputViewConstants.ASK_GET_MORE_BONUS_PRODUCTS;
+import static store.constants.view.OutputViewConstants.ASK_MORE_SHOPPING_PREFIX;
+import static store.constants.view.OutputViewConstants.STORE_INVENTORY_PREFIX;
+import static store.constants.view.OutputViewConstants.USER_ITEM_INPUT_PREFIX;
 import static store.constants.view.ReceiptOutputConstants.RECEIPT_FINAL_TOTAL;
 import static store.constants.view.ReceiptOutputConstants.RECEIPT_MEMBERSHIP_DISCOUNT;
 import static store.constants.view.ReceiptOutputConstants.RECEIPT_PROMOTION_DISCOUNT;
@@ -21,6 +26,7 @@ import static store.constants.view.receipt.ReceiptOutputFormatConstants.RECEIPT_
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Set;
+import store.constants.view.ReceiptOutputConstants;
 import store.domain.Item;
 import store.domain.Product;
 import store.domain.Promotion;
@@ -36,7 +42,8 @@ public class OutputView {
     }
 
     public static void printAskFullPricePurchasePrefix(String itemName, BigDecimal quantity) {
-        System.out.println(ASK_FULL_PRICE_PURCHASE_PREFIX.getOutputViewPrefix().formatted(itemName, quantity.intValue()));
+        System.out.println(
+                ASK_FULL_PRICE_PURCHASE_PREFIX.getOutputViewPrefix().formatted(itemName, quantity.intValue()));
     }
 
     public static void printAskGetMembershipDiscountPrefix() {
@@ -56,11 +63,9 @@ public class OutputView {
     private static void printProduct(Product product) {
         String promotionInfo = getPromotionInfo(product);  // 프로모션이 없으면 공백 출력
 
-        String productOutput = String.format("%s %s %s %s %s", PRODUCT_INDEX.getLabel(),
-                product.getName(),
+        String productOutput = String.format("%s %s %s %s %s", PRODUCT_INDEX.getLabel(), product.getName(),
                 String.format(PRODUCT_PRICE_FORMAT.getFormat(), product.getPrice().intValue()),
-                getProductQuantityFormatted(product),
-                promotionInfo);
+                getProductQuantityFormatted(product), promotionInfo);
         System.out.println(productOutput);
     }
 
@@ -72,50 +77,58 @@ public class OutputView {
     }
 
     private static String getPromotionInfo(Product product) {
-        return product.getPromotion()
-                .map(Promotion::getName)
-                .orElse(PROMOTION_EMPTY.getLabel());
+        return product.getPromotion().map(Promotion::getName).orElse(PROMOTION_EMPTY.getLabel());
     }
 
     public static void printReceipt(List<Item> cart, ReceiptDto receipt) {
         System.out.println(RECEIPT_HEADER.getFormat());
-
-        // 상품 테이블 헤더 출력
         System.out.printf(RECEIPT_TABLE_HEADER.getFormat(), "상품명", "수량", "금액");
+        printItemsInformation(cart);
+        printBonusItems(receipt);
+        System.out.println(RECEIPT_FOOTER.getFormat());
+        printTotalAmountStatistics(receipt);
+    }
 
-        // 상품별 정보 출력
+    private static void printItemsInformation(List<Item> cart) {
         for (Item item : cart) {
             System.out.printf(RECEIPT_ITEM_FORMAT.getFormat(),
                     item.product().getName(),
                     item.quantity(),
-                    item.product().getPrice().intValue());
+                    item.product().getPrice().intValue()
+            );
         }
+    }
 
-        // 증정 품목 출력
+    private static void printTotalAmountStatistics(ReceiptDto receipt) {
+        printTotalAmount(receipt);
+        printPromotionDiscountAmount(receipt);
+        printMembershipDiscountAmount(receipt);
+        printWillPayAmount(RECEIPT_FINAL_TOTAL, receipt.willPayAmounts().intValue());
+    }
+
+    private static void printWillPayAmount(ReceiptOutputConstants receiptFinalTotal, int receipt) {
+        System.out.printf(RECEIPT_TOTAL_FORMAT.getFormat(), receiptFinalTotal.getLabel(), "", receipt);
+    }
+
+    private static void printMembershipDiscountAmount(ReceiptDto receipt) {
+        System.out.printf(RECEIPT_TOTAL_FORMAT.getFormat(), RECEIPT_MEMBERSHIP_DISCOUNT.getLabel(), "",
+                -receipt.membershipDiscountPay().intValue());
+    }
+
+    private static void printPromotionDiscountAmount(ReceiptDto receipt) {
+        System.out.printf(RECEIPT_TOTAL_FORMAT.getFormat(), RECEIPT_PROMOTION_DISCOUNT.getLabel(), "",
+                -receipt.promotionDiscountPay().intValue());
+    }
+
+    private static void printTotalAmount(ReceiptDto receipt) {
+        System.out.printf(RECEIPT_TOTAL_FORMAT.getFormat(), RECEIPT_TOTAL, "", receipt.totalPay().intValue());
+    }
+
+    private static void printBonusItems(ReceiptDto receipt) {
         System.out.println(RECEIPT_PROMOTION_HEADER.getFormat());
         for (Item bonusItem : receipt.promotionBonusItems()) {
-            System.out.printf(RECEIPT_PROMOTION_ITEM_FORMAT.getFormat(),
-                    bonusItem.product().getName(),
+            System.out.printf(RECEIPT_PROMOTION_ITEM_FORMAT.getFormat(), bonusItem.product().getName(),
                     bonusItem.quantity());
         }
-
-        // 영수증 풋터와 합계 출력
-        System.out.println(RECEIPT_FOOTER.getFormat());
-
-        System.out.printf(RECEIPT_TOTAL_FORMAT.getFormat(),
-                RECEIPT_TOTAL.getLabel(), "",
-                receipt.totalPay().intValue());
-
-        System.out.printf(RECEIPT_TOTAL_FORMAT.getFormat(),
-                RECEIPT_PROMOTION_DISCOUNT.getLabel(), "",
-                -receipt.promotionDiscountPay().intValue());
-
-        System.out.printf(RECEIPT_TOTAL_FORMAT.getFormat(),
-                RECEIPT_MEMBERSHIP_DISCOUNT.getLabel(), "",
-                -receipt.membershipDiscountPay().intValue());
-
-        System.out.printf(RECEIPT_TOTAL_FORMAT.getFormat(),
-                RECEIPT_FINAL_TOTAL.getLabel(), "",
-                receipt.willPayAmounts().intValue());
     }
 }
