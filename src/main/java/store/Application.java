ba9afd050ab06aa80f8721763321ecc3fd70abad
qyinm <qusseun@gmail.com>
2024-11-11 09:05:21 +0900

    feat: 예외 발생시 에러 메시지 출력

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -3,10 +3,12 @@ package store;
 import java.util.Map;
 import java.util.Set;
 import store.constants.parser.FileConstants;
+import store.controller.StoreController;
 import store.domain.Product;
 import store.domain.Promotion;
 import store.parser.MarkdownParser;
 import store.repository.StoreRepository;
+import store.service.StoreService;
 
 public class Application {
     public static void main(String[] args) {
@@ -15,6 +17,13 @@ public class Application {
         Set<Product> products = MarkdownParser.parseProducts(FileConstants.PRODUCTIONS_FILE_PATH.getFilePath(),
                 promotionMap);
         StoreRepository storeRepository = new StoreRepository(products);
+        StoreService storeService = new StoreService(storeRepository);
+        StoreController storeController = new StoreController(storeService);
 
+        try {
+            storeController.runStore();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
